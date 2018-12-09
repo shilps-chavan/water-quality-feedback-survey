885a470e6967243424eadfd48aecbb4b74906142
shilps-chavan <45571838+shilps-chavan@users.noreply.github.com>
2018-12-09 22:54:52 +0530

    Update ConnectionManager.java

--- app/database/ConnectionManager.java
+++ app/database/ConnectionManager.java
@@ -3,23 +3,24 @@ package database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 public class ConnectionManager {
     static {
         try {
-            Class.forName("org.postgresql.Driver");
+            Class.forName("org.h2.Driver");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+
     }
+
+
     public  static Connection getConnection()
     {
         Connection c;
+        String url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
 
         try {
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/feedbacksurvey",
-                            "postgres", "admin");
+            c = DriverManager.getConnection(url);
             c.setAutoCommit(false);
         } catch (SQLException e) {
             e.printStackTrace();
